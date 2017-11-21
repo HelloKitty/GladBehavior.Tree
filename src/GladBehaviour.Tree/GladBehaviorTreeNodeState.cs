6009e4c5bce7fbe17980cf8b1cc44e66f9d2e035
HelloKitty <andrew.blakely@ymail.com>
2017-11-21 09:42:52 -0600

    Added tree state enum

---
+++ src/GladBehaviour.Tree/GladBehaviorTreeNodeState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladBehaviour.Tree
+{
+	/// <summary>
+	/// Enumeration of all behavior tree node states.
+	/// </summary>
+	public enum GladBehaviorTreeNodeState : int
+	{
+		/// <summary>
+		/// Indicates that a node is successfully evaluated.
+		/// </summary>
+		Success = 0,
+		
+		/// <summary>
+		/// Indicates that a node is running and will require
+		/// more time for evaluation.
+		/// </summary>
+		Running = 1,
+
+		/// <summary>
+		/// Indicates that a node failed to evaluate.
+		/// </summary>
+		Failure = 2
+	}
+}
