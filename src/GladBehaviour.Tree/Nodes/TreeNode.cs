cc8d168e93985ec4541b9a654f0bbd8d4d462096
HelloKitty <andrew.blakely@ymail.com>
2017-11-21 09:43:08 -0600

    Added base type tree node

---
+++ src/GladBehaviour.Tree/Nodes/TreeNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GladBehaviour.Tree
+{
+	//TODO: Do we need an actual base type or is a interface enough?
+	/// <summary>
+	/// Base type of all nodes in the behavior tree.
+	/// </summary>
+	/// <typeparam name="TContextType">The type of context the node acts on.</typeparam>
+	public abstract class TreeNode<TContextType> : IContextEvaluable<TContextType>
+	{
+		/// <inheritdoc />
+		public abstract GladBehaviorTreeNodeState Evaluate(TContextType context);
+	}
+}
