25493aeefb22fad784aa1c7f54c70d7413c77fb8
HelloKitty <andrew.blakely@ymail.com>
2017-11-21 18:22:10 -0600

    Added IRunningEvaluatable interface
    Interface for types that support async running nodes

---
+++ src/GladBehaviour.Tree/IRunningEvaluatable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladBehaviour.Tree
+{
+	/// <summary>
+	/// Contract for node's that support long running evaluation.
+	/// </summary>
+	/// <typeparam name="TContextType">The context type to be evaluated.</typeparam>
+	public interface IRunningEvaluatable<in TContextType>
+	{
+		/// <summary>
+		/// Indicates if a node is currently running.
+		/// Meaning that the behavior of this evaluation will be circumvenveted
+		/// by a longer-term running node.
+		/// </summary>
+		bool isRunningNode { get; }
+
+		/// <summary>
+		/// The currently running evaluation.
+		/// It's possible that evaluation could take multiple evaluation calls
+		/// to complete. This will be null if <see cref="isRunningNode"/> is false.
+		/// </summary>
+		IContextEvaluable<TContextType> RunningNode { get; }
+	}
+}
